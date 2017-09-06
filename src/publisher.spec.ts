1091298ebf67103bc839ccecc8018644d02c9322
Manuel Mtz-Almeida <manu.valladolid@gmail.com>
2017-09-06 13:32:59 -0500

    style(tests): fix linter warnings

--- src/publisher.spec.ts
+++ src/publisher.spec.ts
@@ -2,12 +2,12 @@ import { Publisher, prepareInterfaces } from './publisher';
 const os = require('os');
 
 const FAKE_INTERFACE = {
-  address: "192.168.0.1",
-  netmask: "255.255.0.0",
-  family: "IPv4",
-  mac: "45:45:45:45:45:45",
+  address: '192.168.0.1',
+  netmask: '255.255.0.0',
+  family: 'IPv4',
+  mac: '45:45:45:45:45:45',
   internal: false,
-  broadcast: "192.168.255.255"
+  broadcast: '192.168.255.255'
 };
 
 describe('publisher', () => {
@@ -40,7 +40,7 @@ describe('publisher', () => {
   it('start/stop', () => {
     const p = new Publisher('devapp', 'conference-app', 8100);
     p.start(() => {
-      fail("not should be called");
+      fail('not should be called');
     });
     expect(p.running).toBeTruthy();
     expect(p.client).toBeDefined();
@@ -54,14 +54,14 @@ describe('publisher', () => {
   it('buildMessage', () => {
     const p = new Publisher('devapp', 'conference-app', 8100);
     const now = Date.now();
-    const message = p.buildMessage("192.168.0.1");
+    const message = p.buildMessage('192.168.0.1');
     const expected = 'ION_DP' + JSON.stringify({
       t: now,
       id: p.id,
       nspace: 'devapp',
       name: 'conference-app',
       host: os.hostname(),
-      ip: "192.168.0.1",
+      ip: '192.168.0.1',
       port: 8100
     });
     expect(message).toBe(expected);
@@ -71,36 +71,36 @@ describe('publisher', () => {
 
 it('prepareInterfaces', () => {
   const iface1 = {
-    address: "192.168.0.2",
-    netmask: "255.255.0.0",
-    family: "IPv4",
-    mac: "45:45:45:45:45:45",
+    address: '192.168.0.2',
+    netmask: '255.255.0.0',
+    family: 'IPv4',
+    mac: '45:45:45:45:45:45',
     internal: false,
   };
   const iface2 = Object.assign({}, iface1);
-  iface2.address = "193.168.0.3";
+  iface2.address = '193.168.0.3';
   iface2.internal = true;
   const iface3 = Object.assign({}, iface1);
-  iface3.address = "194.168.0.4";
+  iface3.address = '194.168.0.4';
   iface3.family = 'IPv6';
   const iface4 = Object.assign({}, iface1);
-  iface4.address = "192.168.0.22";
+  iface4.address = '192.168.0.22';
   const iface5 = Object.assign({}, iface1);
-  iface5.address = "195.168.0.5";
+  iface5.address = '195.168.0.5';
 
   const output = prepareInterfaces({
     'en0': [iface1, iface2, iface3],
     'lo': [iface4, iface5]
   });
   expect(JSON.stringify(output)).toBe(JSON.stringify([{
-    address: "192.168.0.2",
-    broadcast: "192.168.255.255"
+    address: '192.168.0.2',
+    broadcast: '192.168.255.255'
   }, {
-    address: "193.168.0.3",
-    broadcast: "193.168.255.255"
+    address: '193.168.0.3',
+    broadcast: '193.168.255.255'
   }, {
-    address: "195.168.0.5",
-    broadcast: "195.168.255.255"
+    address: '195.168.0.5',
+    broadcast: '195.168.255.255'
   }]));
 });
 
